022fbfcf52d593b4f2b097479587894493ae439f
Maximilian Stroh <Hisako1337@gmail.com>
2017-09-03 19:34:31 +0200

    removed useless abstraction

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,5 +1,4 @@
 import { APIGatewayEvent as Event, Callback as CB, Context } from 'aws-lambda'
-import Handler from './handler'
 import Request from './request'
 import Response from './response'
 
@@ -9,7 +8,22 @@ import Response from './response'
 
 export type Callback = (error: Error, data: Response) => void
 
-export default class API extends Handler {
+export default class API {
+  /**
+   * Configuration for HTTP settings
+   */
+  // url path
+  static path: string
+
+  // specify the desired HTTP Method
+  static method: string = 'GET'
+
+  // add CORS headers and OPTIONS Request for this route
+  static cors: boolean = false
+
+  // more stuff here, like
+  // timeout, ...
+
   // This will be invoked by AWS Lambda. Do not touch.
   static async dispatch(event: Event, context: Context, fn: Callback) {
     try {
@@ -41,5 +55,33 @@ export default class API extends Handler {
   // this function contains your business logic code, will be overriden
   async handle(request: Request): Promise<Response> {
     return new Response(200, '', {})
+  }
+
+  // return data as JSON response
+  json(data: any): Response {
+    const headers = { 'Content-Type': 'application/json; charset=utf-8' }
+    const body = JSON.stringify(data, null, 2)
+    return new Response(200, body, headers)
+  }
+
+  html(data: string): void {
+    // send reponse as HTML
+  }
+
+  text(data: string): Response {
+    const headers = { 'Content-Type': 'text/plain; charset=utf-8' }
+    return new Response(200, data, headers)
+  }
+
+  redirect(url: string): void {
+    // redirect to target url
+  }
+
+  error(message: string): void {
+    // show 500 error message
+  }
+
+  missing(message: string): void {
+    // show 404 error page
   }
 }
