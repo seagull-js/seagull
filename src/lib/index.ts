1211c689624b2eeab544dde7f4bea3a6b26a5704
Tom Jaster <futur3.tom@googlemail.com>
2018-02-04 19:56:15 +0100

    suppress mobx warning

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,12 @@
+// Suppressing warning
+//  [mobx] Warning: there are multiple mobx instances active.
+//  This might lead to unexpected results. See https://github.com/mobxjs/mobx/issues/1082 for details.
+// by isolating state.
+// We get the message during development because cli AND the project do import mobx from different sources.
+// However - the cli does not use anything mobx related so turning this off should be safe.
+import * as mobx from 'mobx'
+mobx.extras.isolateGlobalState()
+
 // API building blocks
 export { default as Request } from './api/request'
 export { default as Response } from './api/response'
