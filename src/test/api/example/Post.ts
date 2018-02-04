632f894916cccd7b8c4c7698d8559149a113a168
Maximilian Stroh <Hisako1337@gmail.com>
2018-02-04 14:36:11 +0100

    small bugfix

--- src/test/api/example/Post.ts
+++ src/test/api/example/Post.ts
@@ -1,16 +1,14 @@
 import { API, Request, Response } from '../../../lib'
 
-export default class Greet extends API {
+export default class Post extends API {
   static method = 'POST'
   static path = '/save'
   async handle(request: Request): Promise<Response> {
-    // tslint:disable-next-line:no-console
-    console.log(request)
     const { name } = request.body
     return this.text(`hello ${name}`)
   }
 }
 
 export const handler = () => {
-  return Greet.dispatch.bind(Greet)
+  return Post.dispatch.bind(Post)
 }
