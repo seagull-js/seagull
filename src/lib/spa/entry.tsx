803dc16d072efdbf58e00a6f8222a761f963480d
Maximilian Stroh <Hisako1337@gmail.com>
2018-02-24 22:27:57 +0100

    auto-start tracking (does nothing if not enabled)

--- src/lib/spa/entry.tsx
+++ src/lib/spa/entry.tsx
@@ -1,14 +1,16 @@
-import { render } from 'react-dom';
+import { render } from 'react-dom'
 import Routing from './routing'
+import Tracking from './tracking'
 
 // client side (browser) entry for loading our seagull app
 async function fetchAndMount() {
   const appRouter = new Routing()
   const page = appRouter.initialMatchedPage()
-  if (page && typeof page.componentDidMount === 'function'){
+  if (page && typeof page.componentDidMount === 'function') {
     await page.componentDidMount()
   }
-  render(appRouter.load(), document.getElementById('root'))  
+  render(appRouter.load(), document.getElementById('root'))
+  new Tracking()
 }
 
 fetchAndMount()
