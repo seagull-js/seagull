c40a48a60ba2e1a31e88393038f1c718e0ced1ad
Tom Jaster <futur3.tom@googlemail.com>
2018-02-26 07:15:10 +0100

    typo

--- src/lib/spa/components/body.tsx
+++ src/lib/spa/components/body.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default ({ children, renderedContent }:{children?, renderedContent:string}) => {
   return (
     <body>
-      <div id='app' dangerouslySetInnerHTML={{__html:renderedContent}}></div>
+      <div id='root' dangerouslySetInnerHTML={{__html:renderedContent}}></div>
       {children}
     </body>
   )
