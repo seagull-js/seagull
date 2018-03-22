e4513d3f965cb67feed11e4d888d3d559a12f293
Tom Jaster <futur3.tom@googlemail.com>
2018-03-22 11:06:22 +0100

    some fmt

--- src/test/spa/components/meta.tsx
+++ src/test/spa/components/meta.tsx
@@ -1,29 +1,43 @@
-import { expect } from 'chai';
-import { skip, slow, suite, test, timeout } from 'mocha-typescript';
-import * as React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import { Helmet } from 'react-helmet';
-import Head from '../../../lib/spa/components/head';
-import Meta from '../../../lib/spa/components/meta';
+import { expect } from 'chai'
+import { skip, slow, suite, test, timeout } from 'mocha-typescript'
+import * as React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { Helmet } from 'react-helmet'
+import Head from '../../../lib/spa/components/head'
+import Meta from '../../../lib/spa/components/meta'
 
 @suite('Meta component')
 class BodyTest {
   @test
   async 'Meta without anything works (does not crash)'() {
-    const content = renderToStaticMarkup(<Meta title={'My title'} />);
+    const content = renderToStaticMarkup(<Meta title={'My title'} />)
     // tslint:disable-next-line:no-console
-    console.log(content);
-    expect(content).to.be.equal('');
+    console.log(content)
+    expect(content).to.be.equal('')
   }
 
   @test
   async 'Meta writes to head'() {
-    const content = renderToStaticMarkup(<><Meta title={'My title'} /><Head /></>);
-    expect(content.indexOf('<title data-react-helmet="true">My title</title>')).to.be.above(0)
+    const content = renderToStaticMarkup(
+      <>
+        <Meta title={'My title'} />
+        <Head />
+      </>
+    )
+    expect(
+      content.indexOf('<title data-react-helmet="true">My title</title>')
+    ).to.be.above(0)
   }
   @test
   async 'Meta children are working'() {
-    const content = renderToStaticMarkup(<><Meta title={'My title'}><meta name='viewport' content='width=device-width, initial-scale=1' /> </Meta><Head/></>);
+    const content = renderToStaticMarkup(
+      <>
+        <Meta title={'My title'}>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />{' '}
+        </Meta>
+        <Head />
+      </>
+    )
     expect(content).to.contain('viewport')
   }
 }
