020b2c743f9337f616739740ccb13020c52e56bd
Maximilian Stroh <Hisako1337@gmail.com>
2018-05-24 22:52:08 +0200

    clean start with first component

---
+++ src/frontend/components/Atom.tsx
@@ -0,0 +1,35 @@
+/** @module Frontend */
+import * as React from 'react'
+
+/**
+ * All properties **must** be primitive values, because the underlying
+ * [[React.PureComponent]] will perform only shallow prop comparison checks
+ * on [[shouldComponentUpdate]].
+ */
+export interface IAtomProps {
+  [propName: string]: string | number | boolean | (() => any)
+}
+
+/**
+ * The smallest possible building block for designing things, this is level 1
+ * in the "Atomic Design" metaphor.
+ * It can only have primitive values as props and no children (except simple
+ * strings), because they are *atomics*, thus not further divisible.
+ * Examples are buttons, text elements, input fields or images.
+ *
+ * Definition: http://atomicdesign.bradfrost.com/outline/#atoms
+ *
+ * Use it like this:
+ *
+ * ```typescript
+ * import { Atom } from '@seagull/framework'
+ * import * as React from 'react'
+ *
+ * export default class Button extends Atom {
+ *   render () {
+ *     return <button onClick={this.props.handler}>{this.props.label}</button>
+ *   }
+ * }
+ * ```
+ */
+export class Atom<P = {}> extends React.PureComponent<IAtomProps> {}
---
+++ test/unit/frontend/components/atom.tsx
@@ -0,0 +1,33 @@
+import { Atom } from '@components/atom'
+import { AtomTest } from '@tdd/atom_test'
+import 'chai/register-should'
+import { skip, slow, suite, test, timeout } from 'mocha-typescript'
+import * as React from 'react'
+
+class Button extends Atom<{ label: string; handler: () => string }> {
+  render() {
+    return <button onClick={e => this.props.handler}>{this.props.label}</button>
+  }
+}
+
+@suite('Unit::Frontend::Components::Atom')
+class Test extends AtomTest<Button> {
+  atom = Button
+
+  @test
+  async 'can be instantiated with props'() {
+    const instance = this.instance({ label: 'label', handler: () => 'click!' })
+    instance.should.be.an('object')
+  }
+
+  @test
+  async 'can be rendered to correct HTML'() {
+    const html = this.html({ label: 'label', handler: () => 'click!' })
+    html.text().should.be.equal('label')
+  }
+
+  @test.skip
+  async 'can be clicked'() {
+    // no idea currently how to test this
+  }
+}
