2c162256a9f2c978d4ce865029a53a1d28595b8b
Maximilian Stroh <Hisako1337@gmail.com>
2018-09-01 20:04:51 +0200

    added missing test to folder deploy

--- test/integration/deploy/folder.ts
+++ test/integration/deploy/folder.ts
@@ -2,6 +2,7 @@ import { Folder } from '@deploy'
 import { AppPlan } from '@scaffold/plans'
 import { listFiles } from '@tools/util'
 import 'chai/register-should'
+import * as fs from 'fs'
 import { skip, slow, suite, test, timeout } from 'mocha-typescript'
 import FunctionalTest from '../../helper/functional_test'
 
@@ -21,7 +22,9 @@ export class Test extends FunctionalTest {
     const deployCommand = new Folder('/tmp/DemoApp')
     await deployCommand.run()
     const files = listFiles('/tmp/DemoApp/public')
-    files.should.include('/tmp/DemoApp/public/index.html')
     files.should.include('/tmp/DemoApp/public/assets/bundle.js')
+    files.should.include('/tmp/DemoApp/public/index.html')
+    const html = fs.readFileSync('/tmp/DemoApp/public/index.html', 'utf-8')
+    html.should.include('<script src="/assets/bundle.js"></script>')
   }
 }
