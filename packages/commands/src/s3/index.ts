50c86ee3088093d90668cc6ba37b27597cc36282
Maximilian Stroh <Hisako1337@gmail.com>
2018-09-28 22:02:24 +0200

    added S3 commands

---
+++ packages/commands/src/s3/index.ts
@@ -0,0 +1,7 @@
+import { ReadFile } from './read_file'
+import { WriteFile } from './write_file'
+
+export const S3 = {
+  ReadFile,
+  WriteFile,
+}
---
+++ packages/commands/src/s3/read_file.ts
@@ -0,0 +1,42 @@
+import { Command } from '@seagull/core'
+import * as AWS from 'aws-sdk'
+
+/**
+ * Command to write File to a specific S3 bucket
+ */
+export class ReadFile implements Command {
+  /**
+   * name of the target bucket
+   */
+  bucketName: string
+
+  /**
+   * Absolute Path to the file including file name and extension
+   */
+  filePath: string
+
+  /**
+   * see the individual property descriptions within this command class
+   */
+  constructor(bucketName: string, filePath: string) {
+    this.bucketName = bucketName
+    this.filePath = filePath
+  }
+
+  /**
+   * write a file to the stack's dataBucket on AWS S3
+   */
+  async execute() {
+    const params = { Bucket: this.bucketName, Key: this.filePath }
+    const client = new AWS.S3()
+    const { Body } = await client.getObject(params).promise()
+    return (Body || '').toString()
+  }
+
+  /**
+   * remove a file from the stack's dataBucket on AWS S3
+   */
+  async revert() {
+    return true
+  }
+}
