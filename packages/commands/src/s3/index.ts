2542220fc4288c43d6331f87ddce684b0ea21d44
Maximilian Stroh <maximilian.stroh@aida.de>
2018-11-06 11:11:52 +0100

    added S3::DeleteFile command

--- packages/commands/src/s3/index.ts
+++ packages/commands/src/s3/index.ts
@@ -1,7 +1,9 @@
+import { DeleteFile } from './delete_file'
 import { ReadFile } from './read_file'
 import { WriteFile } from './write_file'
 
 export const S3 = {
+  DeleteFile,
   ReadFile,
   WriteFile,
 }
