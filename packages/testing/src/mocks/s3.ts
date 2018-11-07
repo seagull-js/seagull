24a89eddf7690487c918358e11894ecab735771e
Maximilian Stroh <maximilian.stroh@aida.de>
2018-11-07 10:57:36 +0100

    testing::S3Mock can now persist itself to disk

--- packages/testing/src/mocks/s3.ts
+++ packages/testing/src/mocks/s3.ts
@@ -1,4 +1,6 @@
 import * as AWSMock from 'aws-sdk-mock'
+import * as fs from 'fs'
+import * as path from 'path'
 import { Mock } from './mock'
 
 /**
@@ -10,6 +12,16 @@ export class S3 implements Mock {
    * this is the place where calls to s3 buckets will be redirected to
    */
   storage: { [Bucket: string]: { [Key: string]: any } } = {}
+
+  /**
+   * When true, save/load the state of storage to local disk
+   */
+  localFolder: string | undefined
+
+  constructor(localFolder?: string) {
+    this.localFolder = localFolder
+    this.loadFromDisk()
+  }
 
   /**
    * redirect S3 interactions to local folder
@@ -36,6 +48,7 @@ export class S3 implements Mock {
   deleteObject = (Input: import('aws-sdk').S3.DeleteObjectRequest, cb: any) => {
     this.ensureBucket(Input.Bucket)
     delete this.storage[Input.Bucket][Input.Key]
+    this.synchronizeToDisk()
     const result: import('aws-sdk').S3.DeleteObjectOutput = {}
     cb(null, result)
   }
@@ -69,6 +82,7 @@ export class S3 implements Mock {
   putObject = (Input: import('aws-sdk').S3.PutObjectRequest, cb: any) => {
     this.ensureBucket(Input.Bucket)
     this.storage[Input.Bucket][Input.Key] = Input.Body
+    this.synchronizeToDisk()
     const result: import('aws-sdk').S3.PutObjectOutput = {}
     cb(null, result)
   }
@@ -76,5 +90,24 @@ export class S3 implements Mock {
   // little helper to ensure that the "bucket" key exists in [[storage]]
   private ensureBucket = (name: string) => {
     this.storage[name] = this.storage[name] || {}
+  }
+
+  private loadFromDisk() {
+    if (this.localFolder) {
+      const filePath = path.join(this.localFolder, 's3.json')
+      if (fs.existsSync(filePath)) {
+        const data = fs.readFileSync(filePath, 'utf-8')
+        this.storage = JSON.parse(data)
+      }
+    }
+  }
+
+  private synchronizeToDisk() {
+    if (this.localFolder) {
+      fs.mkdirSync(this.localFolder)
+      const filePath = path.join(this.localFolder, 's3.json')
+      const fileContent = JSON.stringify(this.storage)
+      fs.writeFileSync(filePath, fileContent, 'utf-8')
+    }
   }
 }
