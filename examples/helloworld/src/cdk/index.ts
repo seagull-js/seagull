d0d2acac84d0488f5f6fd3925a3913ff8b9c44f7
Peter Melinat <peter.melinat@gecko.de>
2018-11-19 09:27:31 +0100

    finished cdk.js

--- examples/helloworld/src/cdk/index.ts
+++ examples/helloworld/src/cdk/index.ts
@@ -1,8 +1,8 @@
 import * as cdk from '@aws-cdk/cdk'
 import * as api from '@aws-cdk/aws-apigateway'
+import * as cf from '@aws-cdk/aws-cloudfront'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as iam from '@aws-cdk/aws-iam'
-
 const pkg = require('../../package.json')
 
 class AppStack extends cdk.Stack {
@@ -17,12 +17,16 @@ class AppStack extends cdk.Stack {
       runtime: lambda.Runtime.NodeJS810,
     }
     const fn = new lambda.Function(this, lambdaName, lambdaParams)
-    const apiGateway = new api.RestApi(this, `${pkg.name}-api-gateway`)
+
+    const apiGateway = new api.RestApi(this, `${pkg.name}-api-gateway`, {
+      binaryMediaTypes: ['*/*'],
+    })
+
     const defaultIntegration = new api.LambdaIntegration(fn)
     apiGateway.root.addMethod('GET', defaultIntegration)
-    const wildCard = apiGateway.root.addResource('{any+}')
-    wildCard.addMethod('GET', defaultIntegration)
-    const role = new iam.Role(this, 'MyRole', {
+    const proxy = apiGateway.root.addResource('{any+}')
+    proxy.addMethod('GET', defaultIntegration)
+    const role = new iam.Role(this, `${pkg.name}-Role`, {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
     })
     // TODO: PLEASE! PLEASE! WORK THIS OVER!!! this could be a viable security breach.
@@ -37,15 +41,38 @@ class AppStack extends cdk.Stack {
           'lambda:InvokeAsync'
         )
     )
+    const cfConfig = {
+      defaultRootObject: '',
+      originConfigs: [
+        {
+          behaviors: [{ isDefaultBehavior: true }],
+          customOriginSource: {
+            domainName: getApiGatewayDomain(apiGateway.url.toString()),
+          },
+          originPath: getApiGatewayPath(apiGateway.url.toString()),
+        },
+      ],
+    }
+    new cf.CloudFrontWebDistribution(this, `${pkg.name}CFD`, cfConfig)
   }
 }
 
 class MyApp extends cdk.App {
   constructor() {
     super()
-
     new AppStack(this, pkg.name)
   }
 }
 
 new MyApp().run()
+
+function getApiGatewayDomain(url: string): string {
+  const noProtocolUrl = url.substring(url.indexOf('://') + 3)
+  return noProtocolUrl.substring(0, noProtocolUrl.indexOf('/'))
+}
+
+function getApiGatewayPath(url: string): string {
+  const noSlashAtEnd = url.endsWith('/') ? url.slice(0, -1) : url
+  const noProtocolUrl = noSlashAtEnd.substring(url.indexOf('://') + 3)
+  return noProtocolUrl.substring(noProtocolUrl.indexOf('/'))
+}
