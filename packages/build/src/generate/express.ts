6770a641e13eab759def90fa30b1ab0fa7b64f22
Maximilian Stroh <Hisako1337@gmail.com>
2018-11-19 22:16:38 +0100

    allow parsing of form post data with array fields

--- packages/build/src/generate/express.ts
+++ packages/build/src/generate/express.ts
@@ -32,7 +32,7 @@ export class Express implements Command {
       'const express = require("express");',
       'const bodyParser = require("body-parser");',
       'const app = express();',
-      'app.use(bodyParser.urlencoded({ extended: false }));',
+      'app.use(bodyParser.urlencoded({ extended: true }));',
       'app.use(bodyParser.json());',
       'app.use(express.static(`${process.cwd()}/dist/assets/static`));',
     ].join('\n')
