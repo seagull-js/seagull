ccc2e58310689cd1cbbe26d2dd521560ef774fb2
Tom Jaster <tom.jaster@gecko.de>
2018-11-30 10:25:56 +0100

    added mock package

---
+++ packages/mock/src/mock.ts
@@ -0,0 +1,15 @@
+/**
+ * The basic interface for implementing the Mock Object Pattern. Use this to
+ * virtualize real world interactions, like database calls or the file system.
+ */
+export interface Mock {
+  /**
+   * Activate the Mock, so subsequent code runs against a fake implementation
+   */
+  activate: () => any
+
+  /**
+   * Deactivate the Mock, so subsequent code runs as usual
+   */
+  deactivate: () => any
+}
