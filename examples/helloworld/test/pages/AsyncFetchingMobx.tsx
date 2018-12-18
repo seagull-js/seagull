535470743ab56c886eb7a691349074b48543bb92
Kjell Sandkuhl <32070654+ksandkuhl@users.noreply.github.com>
2018-12-18 18:51:55 +0100

    Add a generic PageTest class (#36)
    * introduce the PageTest class that provides a recommended way to test pages with further convenience
    
    * new examples for using PageTest added
    
    * pages package also includes tests
    
    * open ToDos:
        - introduce time travel in PageTest e.g. via mocking the clock.
        - add routes for AsyncFetching.tsx, AsyncFetchingMobx.tsx, SecondsLeft.tsx in examples

---
+++ examples/helloworld/test/pages/AsyncFetchingMobx.tsx
@@ -0,0 +1,38 @@
+import { BasicTest } from '../../../../packages/testing/dist/src'
+import 'chai/register-should'
+import { skip, slow, suite, test, timeout } from 'mocha-typescript'
+import * as Enzyme from 'enzyme'
+import * as React from 'react'
+import AsyncFetchingMobx from '../../src/pages/AsyncFetchingMobx'
+import { NetworkLayer } from '../../src/store/NetworkLayer'
+import { PageTest } from '@seagull/pages/src'
+
+const mockData = {
+  products: [
+    { Name: 'Cheese', Price: 2.5, Location: 'Refrigerated foods' },
+    { Name: 'Crisps', Price: 3, Location: 'the Snack isle' },
+    { Name: 'Pizza', Price: 4, Location: 'Refrigerated foods' },
+    { Name: 'Chocolate', Price: 1.5, Location: 'the Snack isle' },
+    { Name: 'Self-raising flour', Price: 1.5, Location: 'Home baking' },
+    { Name: 'Ground almonds', Price: 3, Location: 'Home baking' },
+  ],
+}
+
+@suite('AsyncFetchingMobx')
+export class AsyncFetchingTest extends PageTest {
+  Page = AsyncFetchingMobx
+  @test
+  @timeout(5000)
+  async 'can render page with any data'() {
+    const mockedAsyncFunction = () => Promise.resolve(mockData)
+    const networkLayer = new NetworkLayer(mockedAsyncFunction)
+    const data: any = { someProperty: 'Schinken!' }
+    this.mount({ data, networkLayer })
+    const dataField = this.page.find('#data-field')
+    dataField.text().should.contain(JSON.stringify(data))
+    this.page.find('button').simulate('click')
+    await this.update()
+    const updatedDataField = this.page.find('#data-field')
+    updatedDataField.text().should.contain(JSON.stringify(mockData))
+  }
+}
