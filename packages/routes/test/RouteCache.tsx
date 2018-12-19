3480059f9dbab31a62eb0a2ac4a00a59436b00df
Maik Manthey <maik.manthey@aida.de>
2018-12-19 17:17:31 +0100

    added route caching

---
+++ packages/routes/test/RouteCache.tsx
@@ -0,0 +1,24 @@
+import 'chai/register-should'
+import { skip, slow, suite, test, timeout } from 'mocha-typescript'
+import { Route, RouteTest } from '../src'
+
+class DemoRoute extends Route {
+  static method = 'GET'
+  static path = '/'
+  static cache = 300
+  async handler() {
+    return this.text('demo route')
+  }
+}
+
+@suite('RouteCache')
+export class Test extends RouteTest {
+  route = DemoRoute
+
+  @test
+  async 'setting cache in routes works'() {
+    const { code, data, headers } = await this.invoke('GET', '/', {})
+    code.should.be.equal(200)
+    headers.maxage.should.be.equal(300000)
+  }
+}
