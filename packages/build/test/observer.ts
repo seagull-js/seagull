4748cf6fb25ce3135bd8b4e4417bfb095afd8c4e
Peter Melinat <peter.melinat@gecko.de>
2019-01-02 12:31:16 +0100

    fixed tests, made mode for dev server changable by env MODE

--- packages/build/test/observer.ts
+++ packages/build/test/observer.ts
@@ -23,8 +23,28 @@ export class Test extends BasicTest {
     await observer.initialize()
     const jsFile = await new FS.Exists('/tmp/dist/routes/a.js').execute()
     const jsxFile = await new FS.Exists('/tmp/dist/pages/b.js').execute()
+    Mode.environment.should.be.equals('edge')
+    jsFile.should.be.equal(true)
+    jsxFile.should.be.equal(true)
+  }
+
+  @test
+  async 'can build initial dist folder from scratch and start in pure MODE'() {
+    process.env.MODE = 'pure'
+    const routePath = '/tmp/src/routes/a.ts'
+    const routeContent = 'export default {}'
+    await new FS.WriteFile(routePath, routeContent).execute()
+    const pagePath = '/tmp/src/pages/b.tsx'
+    const pageContent = 'export default {}'
+    await new FS.WriteFile(pagePath, pageContent).execute()
+    await new FS.CreateFolder('/tmp/static').execute()
+    const observer = new Observer('/tmp', { vendor: [] })
+    await observer.initialize()
+    const jsFile = await new FS.Exists('/tmp/dist/routes/a.js').execute()
+    const jsxFile = await new FS.Exists('/tmp/dist/pages/b.js').execute()
     Mode.environment.should.be.equals('pure')
     jsFile.should.be.equal(true)
     jsxFile.should.be.equal(true)
+    delete process.env.MODE
   }
 }
