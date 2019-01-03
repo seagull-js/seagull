0100b149f14848800ac2199439dea4cbdd2b6725
Peter Melinat <peter.melinat@gecko.de>
2019-01-03 16:37:37 +0100

    added iamroles to pipeline

--- packages/pipeline/src/lib/cdk/stack.ts
+++ packages/pipeline/src/lib/cdk/stack.ts
@@ -1,5 +1,6 @@
 import * as CB from '@aws-cdk/aws-codebuild'
 import { GitHubSourceAction, Pipeline } from '@aws-cdk/aws-codepipeline'
+import { PolicyStatement, Role, ServicePrincipal } from '@aws-cdk/aws-iam'
 import { App, Secret, SecretParameter, Stack, StackProps } from '@aws-cdk/cdk'
 import { logNoOwnerFound } from '../log_messages'
 
@@ -11,6 +12,7 @@ export class AppStack extends Stack {
   appPath: string
   pipelineName: string
   pipeline: Pipeline
+  role?: Role
   source!: GitHubSourceAction
 
   constructor(parent: App, pipelineName: string, props: ProjectStackProps) {
@@ -18,6 +20,7 @@ export class AppStack extends Stack {
     this.pipelineName = pipelineName
     this.pipeline = new Pipeline(this, pipelineName, { pipelineName })
     this.appPath = props.env.path
+    this.addIAMRole()
     this.addSourceStage()
     this.addBuildStage()
   }
@@ -32,7 +35,6 @@ export class AppStack extends Stack {
     const sourceConfig = { branch, oauthToken, owner, repo, stage }
     // tslint:disable-next-line:no-unused-expression
     this.source = new GitHubSourceAction(this, 'GitHubSource', sourceConfig)
-    console.info('sourceConfig', sourceConfig)
   }
 
   private addBuildStage() {
@@ -40,28 +42,24 @@ export class AppStack extends Stack {
     const buildImage = CB.LinuxBuildImage.UBUNTU_14_04_NODEJS_8_11_0
     const environment = { buildImage }
     const installCmds = []
-    installCmds.push('echo "Start installing!"')
+    const buildCmds = []
+    const postBuildCmds = []
+
     installCmds.push('npm i -g npm')
     installCmds.push('npm ci')
-    installCmds.push('echo "Finished installing!"')
-    const buildCmds = []
-    buildCmds.push('echo "Start building!"')
     buildCmds.push('npm run build')
-    buildCmds.push('echo "Finished building!"')
-    const postBuildCmds = []
-    postBuildCmds.push('echo "Start testing!"')
     postBuildCmds.push('npm run test')
-    postBuildCmds.push('echo "Finished testing!"')
-
-    postBuildCmds.push('echo "Start deploying!"')
-    postBuildCmds.push('NO_PROFILE_CHECK=true;npm run deploy')
-    postBuildCmds.push('echo "Finished deploying!"')
+    postBuildCmds.push(
+      'export NO_PROFILE_CHECK=true; NO_PROFILE_CHECK=true npm run deploy'
+    )
 
     const install = { commands: installCmds }
     const build = { commands: buildCmds }
     const postBuild = { commands: postBuildCmds }
     const phases = { build, install, post_build: postBuild }
-    const projectConfig = { buildSpec: { phases, version: '0.2' }, environment }
+    const buildSpec = { phases, version: '0.2' }
+    const role = this.role
+    const projectConfig = { buildSpec, environment, role }
     const project = new CB.PipelineProject(this, 'BuildProject', projectConfig)
     const stage = this.pipeline.addStage('BuildStage', { placement })
     const buildProps = { project, stage }
@@ -83,7 +81,6 @@ export class AppStack extends Stack {
   }
 
   private getOwnerPkgJson() {
-    console.info('path', this.appPath)
     const pkgJson = require(`${this.appPath}/package.json`)
     const repoUrl = pkgJson && pkgJson.repository && pkgJson.repository.url
     const isGithubUrl = repoUrl && repoUrl.indexOf('github.com') > -1
@@ -118,6 +115,32 @@ export class AppStack extends Stack {
     const ssmParameter = process.env.GITHUB_SSM_PARAMETER || 'GitHubOAuthToken'
     const secretConfig = { ssmParameter }
     return new SecretParameter(this, 'GithubToken', secretConfig).value
+  }
+
+  private addIAMRole() {
+    const name = `${this.name}-Role`
+    const principleName = 'codebuild.amazonaws.com'
+    const roleParams = { assumedBy: new ServicePrincipal(principleName) }
+    const actions: string[] = []
+    // actions.push('cloudformation:DescribeStacks')
+    // actions.push('cloudformation:CreateChangeSet')
+    // actions.push('cloudformation:DescribeChangeSet')
+    // actions.push('cloudformation:DeleteChangeSet')
+    // actions.push('cloudformation:DescribeStackEvents')
+    // actions.push('cloudformation:DeleteStack')
+    // actions.push('cloudformation:ExecuteChangeSet')
+    actions.push('cloudformation:*')
+    actions.push('cloudfront:*')
+    actions.push('iam:*')
+    actions.push('s3:*')
+    actions.push('apigateway:*')
+    actions.push('lambda:*')
+
+    const role = new Role(this, name, roleParams)
+    const policyStatement = new PolicyStatement()
+    // TODO: add actions directly to the resources that need it
+    role.addToPolicy(policyStatement.addAllResources().addActions(...actions))
+    this.role = role
   }
 }
 
