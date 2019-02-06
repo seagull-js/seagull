8f7dfecf5bf63b5136e3b8f49a101a45a4172678
Tom Jaster <tom.jaster@gecko.de>
2019-02-06 14:28:50 +0100

    some docs

--- packages/routes/src/validation.ts
+++ packages/routes/src/validation.ts
@@ -3,6 +3,7 @@ import { Route } from './Route'
 export const routeIsValid = <T extends typeof Route>(route: T) =>
   pathIsValid(route.path)
 
+// Validates paths like defined in Route
 export const pathIsValid = (path: string) =>
   !!path.match(/^[^\*]*(\/\*)?$/) &&
   !!path.match(/^\/.*/) &&
