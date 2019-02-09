f030a3b32ad4d9662d25e5844abb7be49d8b5066
Tom Jaster <info@tjaster.me>
2019-02-09 14:18:42 +0100

    polyfill fix

--- packages/build/src/bundle/transforms.ts
+++ packages/build/src/bundle/transforms.ts
@@ -2,7 +2,7 @@ import * as browserify from 'browserify'
 
 export const addBabelTransform = (bfy: browserify.BrowserifyObject) => {
   const browsers = ['last 2 versions', 'safari >= 7', 'ie >= 11']
-  const presetEnvOptions = { targets: { browsers }, useBuiltIns: 'entry' }
+  const presetEnvOptions = { targets: { browsers }, useBuiltIns: 'usage' }
   const presets = [['@babel/preset-env', presetEnvOptions]]
   const babelifyOptions = { global: true, presets }
   bfy.transform('babelify', babelifyOptions)
