63d483ca9779b030639e47eb44f5680310f5726b
Tom Jaster <tom.jaster@gecko.de>
2019-02-11 14:53:28 +0100

    ignore core js in babel

--- packages/build/src/bundle/transforms.ts
+++ packages/build/src/bundle/transforms.ts
@@ -4,6 +4,7 @@ export const addBabelTransform = (bfy: browserify.BrowserifyObject) => {
   const browsers = ['last 2 versions', 'safari >= 7', 'ie >= 11']
   const presetEnvOptions = { targets: { browsers }, useBuiltIns: 'usage' }
   const presets = [['@babel/preset-env', presetEnvOptions]]
-  const babelifyOptions = { global: true, presets }
+  const ignore = [/[\/\\]core-js/, /@babel[\/\\]runtime/]
+  const babelifyOptions = { global: true, ignore, presets }
   bfy.transform('babelify', babelifyOptions)
 }
