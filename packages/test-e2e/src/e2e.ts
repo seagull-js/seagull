95c129062b060d730d311547f7545c57c95b273b
Tom Jaster <tom.jaster@gecko.de>
2019-02-13 11:39:21 +0100

    e2e package done

--- packages/test-e2e/src/e2e.ts
+++ packages/test-e2e/src/e2e.ts
@@ -1,25 +1,37 @@
 import * as cucumber from 'cucumber'
 import { existsSync } from 'fs'
+import { flatMap } from 'lodash'
 import { join } from 'path'
-const stdout = process.stdout
+
+// constants we need
 const cwd = process.cwd()
 
-const flatMap = <T, F extends (i: T) => any[]>(collection: T[], func: F) =>
-  collection.map(func).reduce((acc, cv) => acc.concat(cv))
-
+// cucumber should be able to load ts files
 const tsModule = ['--require-module', 'ts-node/register']
+
+// path where world.ts should exist in user project (if override is wanted)
 const projectWorldPath = join(process.cwd(), './features/support/world.ts')
+// determine which world.ts should be used
 const usedWorldPath = existsSync(projectWorldPath)
   ? projectWorldPath
   : require.resolve('./world')
-const stepsPath = './features/step-definitions/**/*.ts'
+// path cucumber step definitions will be in
+const stepsPath = join(cwd, './features/step-definitions/**/*.ts')
+// path cucumber feature definitions will be in
+const featurePath = './features/*.feature'
+// extra recquires for the cucumber cli tool
+const requireArgs = flatMap([stepsPath, usedWorldPath], i => ['--require', i])
 
-const requirePaths = [stepsPath, usedWorldPath]
-const requireArgs = flatMap(requirePaths, i => ['--require', i])
-
-export const e2e = async () => {
+/* 
+  cucumber e2e test runner using zombie js
+  wraps the cucumber cli for more flexibility
+  - Put your features in ./features
+  - Step definitions must be written in typescript and should be placed in
+    ./features/step-definitions/** /*.ts
+  - If you want to override the world create ./features/support/world.ts
+*/
+export const e2e = async ({ stdout } = { stdout: process.stdout }) => {
   const bin = './cucumber'
-  const featurePath = './features/*.feature'
   const argv = [bin, ...tsModule, ...requireArgs, featurePath]
   const cli = new (cucumber as any).Cli({ argv, cwd, stdout })
   const result = await cli.run()
--- packages/test-e2e/test/e2e.ts
+++ packages/test-e2e/test/e2e.ts
@@ -1,13 +1,17 @@
 import 'chai/register-should'
 import { skip, slow, suite, test, timeout } from 'mocha-typescript'
 import * as React from 'react'
+import * as stream from 'stream-buffers'
 import { e2e } from '../src/e2e'
-
 @suite('Page::CustomData')
 export class Test {
   @test
   async 'can render a DemoPage with custom data'() {
+    const stdout = new stream.WritableStreamBuffer()
     // tslint:disable-next-line:no-unused-expression
-    ;(await e2e()).should.be.true
+    ;(await e2e({stdout} as any)).should.be.true
+    const stdoutContent = stdout.getContentsAsString('utf8')
+    stdoutContent.should.contain('1 scenario (1 passed)')
+    stdoutContent.should.contain('2 steps (2 passed)')
   }
 }
