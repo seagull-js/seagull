3c29eefcebeb7b6670e82b56bbb1922e647a7192
Maik Manthey <maik.manthey@aida.de>
2019-02-27 15:33:03 +0100

    added missing promise()

--- packages/commands-logging/src/write_log.ts
+++ packages/commands-logging/src/write_log.ts
@@ -61,10 +61,12 @@ export class WriteLog extends Command<
   }
 
   private async exec(client: AWS.CloudWatchLogs) {
-    await client.createLogStream({
-      logGroupName: this.params.logGroupName,
-      logStreamName: this.params.logGroupName,
-    })
+    await client
+      .createLogStream({
+        logGroupName: this.params.logGroupName,
+        logStreamName: this.params.logGroupName,
+      })
+      .promise()
     console.info('this.params', this.params)
     const result = await client.putLogEvents(this.params).promise()
     sequenceToken = result.nextSequenceToken
