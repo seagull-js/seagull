124a73a2cc0cb5f91faf319d6b817da9dc27320c
Christian Pätzold <cpa@gecko.de>
2019-03-08 10:25:27 +0100

    quick fix : failing e2e test timeout raised

--- packages/test-e2e/test/e2e.ts
+++ packages/test-e2e/test/e2e.ts
@@ -5,13 +5,12 @@ import * as stream from 'stream-buffers'
 import { e2e } from '../src/e2e'
 @suite('TestE2E::Simple e2e test')
 export class Test {
-  
   @test
-  @timeout(10000)
+  @timeout(30000)
   async 'cli picks ups scenarios and executes steps'() {
     const stdout = new stream.WritableStreamBuffer()
-    // tslint:disable-next-line:no-unused-expression
-    ;(await e2e({stdout} as any)).should.be.true
+      // tslint:disable-next-line:no-unused-expression
+    ;(await e2e({ stdout } as any)).should.be.true
     const stdoutContent = stdout.getContentsAsString('utf8')
     stdoutContent.should.contain('1 scenario (1 passed)')
     stdoutContent.should.contain('2 steps (2 passed)')
