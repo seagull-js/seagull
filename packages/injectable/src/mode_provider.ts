6aaeba2b04f33ab08bcf5541786864c69fb70366
Christian Pätzold <cpa@gecko.de>
2019-03-18 17:25:42 +0100

    injectable - documentation added

--- packages/injectable/src/mode_provider.ts
+++ packages/injectable/src/mode_provider.ts
@@ -1,15 +1,15 @@
 import { Environments, Mode } from '@seagull/mode'
 import { ClassProvider, Type } from 'injection-js'
 
-export type ModeProviders<T> = { [environment in Environments]: Type<T> }
 /**
- * The Basic interface for implementing the Command-Pattern. Commands are
- * responsible for performing code with side-effects. Benefits over raw
- * functions are: explicit support for undoing things and simple queueing
- * of mulitple commands.
- *
- * All Parameters shall be passed in during initialization of the object but
- * never directly on the [[execute]] or [[revert]] methods.
+ * Dictionary listing injection-js providers for all seagull mode environments.
+ */
+export type ModeProviders<T> = { [environment in Environments]: Type<T> }
+
+/**
+ * Creates an injection class provider depending on the current seagull mode
+ * environment.
+ * @param modeProviders ModeProviders dictionary.
  */
 export const createModeProvider = <T>(
   modeProviders: ModeProviders<T>
