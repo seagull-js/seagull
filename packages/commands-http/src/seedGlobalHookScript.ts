f4c8b82614a2fa42efd130244a5e81bff7cb8e5e
Christian Pätzold <cpa@gecko.de>
2019-03-21 16:41:48 +0100

    commands-http removed

--- packages/commands-http/src/seedGlobalHookScript.ts
+++
@@ -1,8 +0,0 @@
-export const seedGlobalHookScript = <T>(fixture: T): T => {
-  if (Array.isArray(fixture)) {
-    // cut all after first 2
-    return fixture.slice(0, 2) as any
-  } else {
-    return fixture
-  }
-}
