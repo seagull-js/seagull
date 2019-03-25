d74410f62da1dec90dc92acca1d94b77db5aa023
Christian Pätzold <cpa@gecko.de>
2019-03-25 15:31:02 +0100

    debug code removed

--- packages/routes/test/RouteApiKey.tsx
+++ packages/routes/test/RouteApiKey.tsx
@@ -1,18 +1,15 @@
 import 'chai/register-should'
+import { blockParams } from 'handlebars'
 import { skip, slow, suite, test, timeout } from 'mocha-typescript'
 import { Route, RouteContext, RouteTest } from '../src'
-import { blockParams } from 'handlebars';
 
 class DemoRoute extends Route {
   static apiKey = '2135t'
   static method = 'GET'
   static path = '/'
   static cache = 300
-  static injector: new Container
 
   static async handler(this: RouteContext) {
-    DemoRoute.injector
-    injector = new Container()
     this.text('hey' + this.request.url)
   }
 }
@@ -23,8 +20,7 @@ export class Test extends RouteTest {
 
   @test
   async 'no token does not work'() {
-    blockParams.bind(..)
-    const { code, data, headers } = await this.invoke('GET', '/', {}, { injector: bla})
+    const { code, data, headers } = await this.invoke('GET', '/', {})
     code.should.be.equal(500)
   }
 
