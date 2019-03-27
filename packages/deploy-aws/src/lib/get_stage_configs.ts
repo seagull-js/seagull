d1b8b4a93eab4e9d125075b21f6a5ce7a80a2f5c
Peter Melinat <peter.melinat@gecko.de>
2019-03-27 17:48:29 +0100

    fixed pipeline

--- packages/deploy-aws/src/lib/get_stage_configs.ts
+++ packages/deploy-aws/src/lib/get_stage_configs.ts
@@ -13,6 +13,8 @@ interface StageConfigParams {
   ssmSecret: { name: string; secret: Secret }
 }
 
+const curlData = `-d '{ "state": "'$PIPELINE_STATE'", "target_url": "'$TARGET_URL'", "description": "'"$PIPELINE_DESC"' - Seagull Test CI", "context": "'$TEST_PIPELINE_CONTEXT'"}'`
+
 export function getSourceConfig(params: StageConfigParams, index: number) {
   return {
     atIndex: index,
@@ -37,7 +39,7 @@ export function getBuildConfig(params: StageConfigParams, index: number) {
 }
 
 function getInstall(params: StageConfigParams) {
-  const curlCmd = getCurlToSendTestResult(params.owner, params.repo)
+  const curlCmd = getCurlToSendTestResult(params.owner, params.repo, curlData)
   const upgradeNpm = addStateChangeToCmd('npm i -g npm')
   const runInstall = addStateChangeToCmd('npm ci')
   const commands = [curlCmd, upgradeNpm, checkState(), runInstall, checkState()]
@@ -45,19 +47,20 @@ function getInstall(params: StageConfigParams) {
 }
 
 function getBuild(params: StageConfigParams) {
-  const curlCmd = getCurlToSendTestResult(params.owner, params.repo)
+  const curlCmd = getCurlToSendTestResult(params.owner, params.repo, curlData)
   const runBuild = addStateChangeToCmd('npm run build')
   return { commands: [runBuild, checkState()], finally: [curlCmd] }
 }
 
 function getPostBuild(params: StageConfigParams) {
-  const curlCmd = getCurlToSendTestResult(params.owner, params.repo)
+  const curlCmd = getCurlToSendTestResult(params.owner, params.repo, curlData)
   const commands = [
     addStateChangeToCmd('npm run test'),
     checkState(),
     addStateChangeToCmd('npm run deploy'),
     checkState(),
-    setEnvAfterDeploy(),
+    'export CFURL="https://$(cat /tmp/cfurl.txt)"',
+    sendDeploymentInfo(params.owner, params.repo),
     addStateChangeToCmd(`npm run test:e2e`),
     checkState(),
     `export PIPELINE_STATE="success";export PIPELINE_DESC="successful";`,
@@ -65,30 +68,26 @@ function getPostBuild(params: StageConfigParams) {
   return { commands, finally: [curlCmd] }
 }
 
-function setEnvAfterDeploy() {
-  const setCFURL = `export CFURL=$(cat /tmp/cfurl.txt);`
-  const setTarget = `export TARGET_URL=$(cat /tmp/cfurl.txt);`
-  const setDesc = `export PIPELINE_DESC="repository was successfully deployed";`
-  const setContext = `export TEST_PIPELINE_CONTEXT="continuous-integration/seagull-deployment";`
-  return `${setCFURL}${setTarget}${setDesc}${setContext}`
+function sendDeploymentInfo(owner: string, name: string) {
+  const data = `-d '{ "state": "'"success"'", "target_url": "'"https://$(cat /tmp/cfurl.txt)"'", "description": "'"repository was successfully deployed"' - Seagull Test CI", "context": "'"continuous-integration/seagull-deployment"'"}'`
+  return getCurlToSendTestResult(owner, name, data)
 }
 
 function checkState() {
-  return `if [ "$PIPELINE_STATE" = "failure" ]; then exit 1337; fi`
+  return `if [ "$PIPELINE_STATE" = "failure" ]; then exit 1; fi`
 }
 
 function addStateChangeToCmd(cmd: string) {
   return `export PIPELINE_DESC='${cmd}'; if ${cmd}; then echo "success at ${cmd}!"; else export PIPELINE_STATE="failure"; fi`
 }
 
-function getCurlToSendTestResult(owner: string, name: string) {
+function getCurlToSendTestResult(owner: string, name: string, data: string) {
   const curlDomain = 'https://api.github.com'
   const curlPath = `/repos/${owner}/${name}/statuses/$CODEBUILD_RESOLVED_SOURCE_VERSION`
   const curlParams = `?access_token=$ACCESS_TOKEN`
   const curlUrl = `"${curlDomain}${curlPath}${curlParams}"`
   const curlHeaders = `-H 'Content-Type: application/json'`
-  const curlData = `-d '{ "state": "'$PIPELINE_STATE'", "target_url": "'$TARGET_URL'", "description": "'"$PIPELINE_DESC"' - Seagull Test CI", "context": "'$TEST_PIPELINE_CONTEXT'"}'`
-  return `curl -g -X POST ${curlUrl} ${curlHeaders} ${curlData}`
+  return `curl -g -X POST ${curlUrl} ${curlHeaders} ${data}`
 }
 
 function getEnv(params: StageConfigParams) {
--- packages/test-e2e/bin/e2e.js
+++ packages/test-e2e/bin/e2e.js
@@ -5,7 +5,8 @@ const {
   e2e
 } = require('../dist/src/e2e')
 
-const logSuccess = () => console.log('done')
+const logResult = (result) => result === false && process.exit(1)
+
 const logError = (error) => console.log('error', error) && process.exit(1)
 
-e2e().then(() => logSuccess()).catch(error => logError(error))
+e2e().then((result) => logResult(result)).catch(error => logError(error))
