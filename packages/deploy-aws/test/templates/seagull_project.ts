ae4928e9d6c3954f5cdc728df1d95a73a8edf0d7
Kjell Sandkuhl <kjell.sandkuhl@gecko.de>
2019-03-28 12:03:39 +0100

    Test the expected behaviour of costomizing aws infrastructure

--- packages/deploy-aws/test/templates/seagull_project.ts
+++ packages/deploy-aws/test/templates/seagull_project.ts
@@ -4,15 +4,33 @@ import { BasicTest } from '@seagull/testing'
 import { expect, use } from 'chai'
 import * as promisedChai from 'chai-as-promised'
 import 'chai/register-should'
-import { unlinkSync, writeFileSync } from 'fs'
+import { readFileSync, unlinkSync, writeFileSync } from 'fs'
 import { cloneDeep, find } from 'lodash'
-import { suite, test } from 'mocha-typescript'
+import { suite, test, timeout } from 'mocha-typescript'
 import { SeagullApp, SeagullProject } from '../../src'
 import { isInList } from '../test-helper/template_searching'
 use(promisedChai)
+
+const s3Name = 'another-s3'
+const customizationCode = `import { SeagullApp } from '../src'
+export default function(app: SeagullApp) {
+  app.stack.addS3('another-s3', app.role)
+}`
+
 @suite('SeagullProject')
 export class Test extends BasicTest {
   appPath = `${process.cwd()}/test_data`
+  projectProps = {
+    accountId: 'test-account-id',
+    appPath: this.appPath,
+    branch: 'master',
+    githubToken: 'Token123',
+    mode: 'prod',
+    owner: 'me',
+    profile: 'default',
+    region: 'eu-central-1',
+    repository: 'test-repo',
+  }
   async before() {
     await BasicTest.prototype.before.bind(this)()
     process.env.AWS_REGION = 'eu-central-1'
@@ -33,53 +51,22 @@ export class Test extends BasicTest {
   }
   @test
   async 'can create a project'() {
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const project = await new SeagullProject(props).createSeagullApp()
+    const project = await new SeagullProject(
+      this.projectProps
+    ).createSeagullApp()
     const synthStack = project.synthesizeStack('helloworld')
     Object.keys(synthStack.template.Resources).length.should.be.above(1)
   }
 
   @test
   async 'assigns a default role to role property of SeagullApp'() {
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const app = await new SeagullProject(props).createSeagullApp()
+    const app = await new SeagullProject(this.projectProps).createSeagullApp()
     expect(app.role).to.be.instanceOf(Role)
   }
 
   @test
   async 'can add policies after creating SeagullApp'() {
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const app = await new SeagullProject(props).createSeagullApp()
+    const app = await new SeagullProject(this.projectProps).createSeagullApp()
     app.role!.addToPolicy(
       new PolicyStatement().addAllResources().addAction('action3')
     )
@@ -91,25 +78,9 @@ export class Test extends BasicTest {
 
   @test
   async 'can create a project and customize stack'() {
-    const s3Name = 'another-s3'
-    const infra = `import { SeagullApp } from '../src'
-    export default function(app: SeagullApp) {
-      app.stack.addS3('another-s3', app.role)
-    }`
-    writeFileSync(`${this.appPath}/infrastructure-aws.ts`, infra)
+    writeFileSync(`${this.appPath}/infrastructure-aws.ts`, customizationCode)
 
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const project = new SeagullProject(props)
+    const project = new SeagullProject(this.projectProps)
     const app = await project.createSeagullApp()
     const stackName = 'helloworld'
     await project.customizeStack(app)
@@ -125,13 +96,29 @@ export class Test extends BasicTest {
     unlinkSync(`${this.appPath}/infrastructure-aws.ts`)
   }
 
+  /** This test needs to use another directory, because imports are cached
+   * and here the import of infrastructure-aws.ts needs to be something
+   * other than the infrastructure-aws.ts above */
   @test
   async 'customize stack throws when infrastructure-aws.ts is corrupted'() {
-    writeFileSync(`${this.appPath}/infrastructure-aws.ts`, '')
+    const assetFolder = `${this.appPath}/corrupted-infrastructure/dist/assets`
+    const backendFolder = `${assetFolder}/backend`
+    const createBackendFolder = new FS.CreateFolder(backendFolder)
+    await createBackendFolder.execute()
+    await new FS.WriteFile(`${backendFolder}/server.js`, '').execute()
+    await new FS.WriteFile(`${backendFolder}/lambda.js`, '').execute()
+    await new FS.WriteFile(
+      `${this.appPath}/corrupted-infrastructure/dist/cron.json`,
+      JSON.stringify([])
+    ).execute()
+    writeFileSync(
+      `${this.appPath}/corrupted-infrastructure/infrastructure-aws.ts`,
+      'export default 123'
+    )
 
     const props = {
       accountId: 'test-account-id',
-      appPath: this.appPath,
+      appPath: `${this.appPath}/corrupted-infrastructure`,
       branch: 'master',
       githubToken: 'Token123',
       mode: 'prod',
@@ -140,28 +127,18 @@ export class Test extends BasicTest {
       region: 'eu-central-1',
       repository: 'test-repo',
     }
+
     const project = new SeagullProject(props)
     const app = await project.createSeagullApp()
 
-    const customize = async () => await project.customizeStack(app)
-    expect(customize).to.be.rejectedWith(Error)
-    unlinkSync(`${this.appPath}/infrastructure-aws.ts`)
+    const customize = () => project.customizeStack(app)
+    await expect(customize()).to.be.rejectedWith(Error)
+    unlinkSync(`${this.appPath}/corrupted-infrastructure/infrastructure-aws.ts`)
   }
 
   @test
   async 'customizeStack should do nothing but return false without an infrastructure-aws.ts-file'() {
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const project = new SeagullProject(props)
+    const project = new SeagullProject(this.projectProps)
     const app = await project.createSeagullApp()
     const appBeforeCustomization = cloneDeep(app)
     const returnValue = await project.customizeStack(app)
@@ -171,18 +148,7 @@ export class Test extends BasicTest {
 
   @test
   async 'can deploy a project without customized stack'() {
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const project = new SeagullProject(props)
+    const project = new SeagullProject(this.projectProps)
     let hasBeenCalled = false
     const deployStack = () => (hasBeenCalled = true)
     project.createSeagullApp = () =>
@@ -195,25 +161,9 @@ export class Test extends BasicTest {
 
   @test
   async 'can deploy a project with customized stack'() {
-    const s3Name = 'another-s3'
-    const infra = `import { SeagullApp } from '../src'
-    export default function(app: SeagullApp) {
-      app.stack.addS3('another-s3', app.role)
-    }`
-    writeFileSync(`${this.appPath}/infrastructure-aws.ts`, infra)
+    writeFileSync(`${this.appPath}/infrastructure-aws.ts`, customizationCode)
 
-    const props = {
-      accountId: 'test-account-id',
-      appPath: this.appPath,
-      branch: 'master',
-      githubToken: 'Token123',
-      mode: 'prod',
-      owner: 'me',
-      profile: 'default',
-      region: 'eu-central-1',
-      repository: 'test-repo',
-    }
-    const project = new SeagullProject(props)
+    const project = new SeagullProject(this.projectProps)
     let hasBeenCalled = false
     const deployStack = async () => {
       hasBeenCalled = true
@@ -241,6 +191,54 @@ export class Test extends BasicTest {
     s3InMeta.should.be.equal(true)
 
     hasBeenCalled.should.be.equal(true)
+    unlinkSync(`${this.appPath}/infrastructure-aws.ts`)
+  }
+
+  @test
+  async 'can diff a project without customized stack'() {
+    const project = new SeagullProject(this.projectProps)
+    let hasBeenCalled = false
+    const diffStack = () => (hasBeenCalled = true)
+    project.createSeagullApp = () =>
+      Promise.resolve(({ diffStack } as unknown) as SeagullApp)
+
+    await project.diffProject()
+
+    expect(hasBeenCalled).to.be.equal(true)
+  }
+
+  @test
+  async 'can diff a project with customized stack'() {
+    writeFileSync(`${this.appPath}/infrastructure-aws.ts`, customizationCode)
+
+    const project = new SeagullProject(this.projectProps)
+    let hasBeenCalled = false
+    const diffStack = async () => {
+      hasBeenCalled = true
+    }
+    const createSeagullApp = project.createSeagullApp
+    let app: SeagullApp
+    project.createSeagullApp = async () => {
+      app = await createSeagullApp.bind(project)()
+      app.diffStack = diffStack
+      return app
+    }
+    const stackName = 'helloworld'
+
+    await project.diffProject()
+
+    const synthStack = app!.synthesizeStack(stackName)
+    const resources = Object.keys(synthStack.template.Resources)
+    const metadata = Object.keys(synthStack.metadata)
+    const stackNameNoDash = stackName.replace(/-/g, '')
+    const s3NameNoDash = s3Name.replace(/-/g, '')
+    const s3InTemp = isInList(resources, s3NameNoDash, stackNameNoDash)
+    const s3InMeta = isInList(metadata, s3Name, stackName)
+    s3InTemp.should.be.equal(true)
+    s3InMeta.should.be.equal(true)
+
+    hasBeenCalled.should.be.equal(true)
+    unlinkSync(`${this.appPath}/infrastructure-aws.ts`)
   }
 }
 
--- packages/deploy-aws/test_data/infrastructure-aws.ts
+++
@@ -1,4 +0,0 @@
-import { SeagullApp } from '../src'
-    export default function(app: SeagullApp) {
-      app.stack.addS3('another-s3', app.role)
-    }
