c981fef5eed6927aa496903137b9fd324c4194c1
Peter Melinat <peter.melinat@gecko.de>
2019-04-03 17:15:01 +0200

    fixed missing flag for compression in cloudfront

--- packages/deploy-aws/src/seagull_stack.ts
+++ packages/deploy-aws/src/seagull_stack.ts
@@ -81,10 +81,13 @@ export class SeagullStack extends Stack {
     const name = `${this.id}-${cfdName}`
     const domainName = getApiGatewayDomain(props.apiGateway.url)
     const originPath = getApiGatewayPath(props.apiGateway.url)
-    const allowedMethods = CF.CloudFrontAllowedMethods.ALL
-    const forwardedValues = { headers: ['authorization'], queryString: true }
-    const isDefaultBehavior = true
-    const behaviors = [{ allowedMethods, forwardedValues, isDefaultBehavior }]
+    const defaultBehavior = {
+      allowedMethods: CF.CloudFrontAllowedMethods.ALL,
+      compress: true,
+      forwardedValues: { headers: ['authorization'], queryString: true },
+      isDefaultBehavior: true,
+    }
+    const behaviors = [defaultBehavior]
     const customOriginSource = { domainName }
     const conf = {
       aliasConfiguration: props.aliasConfig,
