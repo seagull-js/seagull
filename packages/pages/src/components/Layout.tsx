353304a0194ed809939da46ca20c5c16746d8864
Christian Pätzold <cpa@gecko.de>
2019-04-11 17:10:54 +0200

    helmet scripts moved to header / currently unsupported props removed from HelmetData

--- packages/pages/src/components/Layout.tsx
+++ packages/pages/src/components/Layout.tsx
@@ -1,7 +1,9 @@
+import * as _ from 'lodash'
 import * as React from 'react'
+import { HelmetData } from '../helmet'
 
 export interface ILayoutProps {
-  helmet?: import('react-helmet').HelmetData
+  helmet?: HelmetData
   data?: any
   children: React.ReactNode
   pageBundle?: string
@@ -10,20 +12,21 @@ export interface ILayoutProps {
 
 export class Layout extends React.Component<ILayoutProps> {
   render() {
+    const helmet = this.props.helmet
+    const styles = this.props.styles || ''
     return (
       <html prefix="og: http://ogp.me/ns#">
         <head>
-          {this.props.helmet ? this.props.helmet.meta.toComponent() : ''}
-          {this.props.helmet ? this.props.helmet.title.toComponent() : ''}
-          {this.props.helmet ? this.props.helmet.link.toComponent() : ''}
-          {this.props.helmet ? this.props.helmet.style.toComponent() : ''}
-          {this.props.styles && (
-            <style id="styles-target">{this.props.styles || ''}</style>
-          )}
+          {helmet ? helmet.meta.toComponent() : ''}
+          {helmet ? helmet.title.toComponent() : ''}
+          {helmet ? helmet.link.toComponent() : ''}
+          {helmet ? helmet.style.toComponent() : ''}
+          {styles && <style id="styles-target">{styles}</style>}
+          {helmet ? helmet.script.toComponent() : ''}
         </head>
         <body>
+          <noscript title="noscript-the-one-and-only" />
           <div id="app">{this.props.children}</div>
-
           {this.props.data && (
             <script
               dangerouslySetInnerHTML={{
@@ -38,7 +41,6 @@ export class Layout extends React.Component<ILayoutProps> {
             dangerouslySetInnerHTML={{ __html: this.props.pageBundle || '' }}
           />
           <script>window.Page.default.bootstrap()</script>
-          {this.props.helmet ? this.props.helmet.script.toComponent() : ''}
         </body>
       </html>
     )
