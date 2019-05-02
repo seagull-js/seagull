4405466b764c0e101911b65efa5f6c989a05dc7e
Christian Pätzold <cpa@gecko.de>
2019-05-02 13:25:42 +0200

    some documentation added

--- packages/libraries/src/basicError.ts
+++ packages/libraries/src/basicError.ts
@@ -1,7 +1,16 @@
+/**
+ * Basic error \
+ * _shorthand for creating named JS errors_
+ */
 export class BasicError extends Error {
+  /**
+   * Creates a new basic error.
+   * @param name A name for the type of error. Should be different to JS default errors.
+   * @param message A human-readable description of the error.
+   */
   constructor(
     /**
-     * Define a name for the type of error. Should be different to JS default errors.
+     * A name for the type of the error. Should be different to JS default errors.
      * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
      */
     name: string,
--- packages/seed/src/error.ts
+++ packages/seed/src/error.ts
@@ -2,6 +2,11 @@ import { BasicError } from '@seagull/libraries'
 import { FixtureStorage } from './fixtureStorage'
 
 export class SeedError extends BasicError {
+  /**
+   * Creates a new seed error
+   * @param message human readable error message
+   * @param seed seed fixture storage
+   */
   constructor(message: string, public seed: FixtureStorage<any> | any) {
     super('SeedError', message)
   }
