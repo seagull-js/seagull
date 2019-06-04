a60b9ee5276ceab983fc071334716d4f5c2300f5
DfHlyu4aida <46778022+DfHlyu4aida@users.noreply.github.com>
2019-06-04 14:12:21 +0200

    chore(deploy-aws): destroy project when pipeline gets destroyed

--- packages/deploy-aws/bin/destroy-pipeline.js
+++ packages/deploy-aws/bin/destroy-pipeline.js
@@ -2,13 +2,23 @@
 
 const logSuccess = () => console.log('done')
 const logError = error => console.log('error', error) && process.exit(1)
-const { SeagullPipeline } = require('../dist/src/templates/seagull_pipeline')
+const {
+  SeagullPipeline,
+  SeagullProject,
+} = require('../dist/src/templates/seagull_pipeline')
 
-const options = require('./_pipeline-options')
+const pipelineOptions = require('./_pipeline-options')
+const projectOptions = require('./_project-options')
 
 process.env.AWS_REGION = options.region
 
-const pipeline = new SeagullPipeline(options)
+const project = new SeagullProject(projectOptions)
+project
+  .destroyProject()
+  .then(() => logSuccess())
+  .catch(error => logError(error))
+
+const pipeline = new SeagullPipeline(pipelineOptions)
 pipeline
   .destroyPipeline()
   .then(() => logSuccess())
