60dbbd7e25435deede820448b14d79f6b59e8fff
Tom Jaster <tom.jaster@gecko.de>
2019-06-05 10:05:27 +0200

    fixed require path

--- packages/deploy-aws/bin/destroy-pipeline.js
+++ packages/deploy-aws/bin/destroy-pipeline.js
@@ -5,7 +5,7 @@ const logError = error => console.log('error', error) && process.exit(1)
 const {
   SeagullPipeline,
   SeagullProject,
-} = require('../dist/src/templates/seagull_pipeline')
+} = require('../dist/src/templates')
 
 const pipelineOptions = require('./_pipeline-options')
 const projectOptions = require('./_project-options')
