9878a88aef114efe7f0e77342a975554ea6050c7
Kevin Schwedersky <kevin.schwedersky@aida.de>
2019-07-23 08:35:02 +0200

    Fixed sample error route

--- examples/helloworld/src/routes/error.ts
+++ examples/helloworld/src/routes/error.ts
@@ -1,7 +1,7 @@
 import { Route, RouteContext } from '@seagull/routes'
 
 export default class PageRoute extends Route {
-  static path = '/error'
+  static path = '/failure'
   static async handler(this: RouteContext) {
     this.error('Goodbye, cruel world.')
   }
