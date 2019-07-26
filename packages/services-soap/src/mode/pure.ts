5c835c580c5c4ac6aacd795a880c4c6e8b4eb85c
Kjell Sandkuhl <kjell.sandkuhl@gecko.de>
2019-07-26 14:40:39 +0200

    first attempt to implement SoapClientSupplierPure

--- packages/services-soap/src/mode/pure.ts
+++ packages/services-soap/src/mode/pure.ts
@@ -1,20 +1,38 @@
 import { FixtureStorage } from '@seagull/seed'
 import { injectable } from 'inversify'
+import { flatMap, map } from 'lodash'
+import fetch from 'node-fetch'
 import 'reflect-metadata'
 import * as soap from 'soap'
-import { ClientOptions } from '..'
+import { ClientOptions, Credentials } from '..'
 import { SoapClientSupplierBase } from './base'
 
+const replaceMethod = (client: soap.Client, meth: string, wsdlPath: string) => {
+  client[meth] = async (...params: any) =>
+    FixtureStorage.createByFetchParams(`${wsdlPath}/${meth}`, params).get()
+}
+
+const getAsyncMethods = (client: soap.Client) => {
+  const { bindings } = client.wsdl.definitions
+  return flatMap(bindings, b =>
+    Object.keys(b.topElements).map(meth => `${meth}Async`)
+  )
+}
+
+const purifyClient = (client: soap.Client, wsdlPath: string) => {
+  const asyncMeths: string[] = getAsyncMethods(client)
+  asyncMeths.forEach(meth => replaceMethod(client, meth, wsdlPath))
+  return client
+}
 /**
- * Soap client supplier pure mode implementation.
+ * Soap client supplier seed mode implementation.
  */
 @injectable()
 export class SoapClientSupplierPure extends SoapClientSupplierBase {
-  getClient<T extends soap.Client>(options: ClientOptions): Promise<T> {
-    const client = this.getClientInternal(options)
-    // TODO: replace client generated functions with adapter doing something like:
-    // const seed = FixtureStorage.createByFetchParams<Fixture<any>>(url, init)
-    // const fixture = seed.get()
-    throw new Error('not implemented')
+  async getClient<T extends soap.Client>(options: ClientOptions): Promise<T> {
+    const wsdlPath = `seed/${options.wsdlPath}.wsdl`.replace('://', '/')
+    const client = await this.getClientInternal({ ...options, wsdlPath })
+    // TODO: replace client generated functions with adapter doint the request and replacing the fixture
+    return purifyClient(client, options.wsdlPath) as T
   }
 }
